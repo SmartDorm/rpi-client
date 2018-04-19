51538782ca0b9d53c539285b5e9b8019986c1031
Cole Nelson <nelso345@purdue.edu>
2018-04-18 22:50:06 -0400

    WEATHER + HEADLINES WORKING

--- sdClient.py
+++ sdClient.py
@@ -1,5 +1,7 @@
 import urllib.request
 import json
+import requests
+import random
 import pprint as pp
 
 def getWeather():
@@ -16,9 +18,35 @@ def getWeather():
 
     return weather_dict
 
+def createResultString(response_dict):
+    high = str(response_dict.get('main').get('temp_max'))
+    low = str(response_dict.get('main').get('temp_min'))
+    current = str(response_dict.get('main').get('temp'))
+    conditions = response_dict['weather'][0]['main']
+
+    return high + "," + low + "," + current + "," + conditions
+
+def getNews():
+    url = ('https://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'apiKey=a8fc51c4713b4112973d8b577bc5e694')
+
+    response = requests.get(url)
+    parsed = json.loads(response.text)
+
+    titles = []
+
+    for i in parsed.get("articles"):
+        titles.append(i.get("title"))
+
+    return titles
+
 if __name__ == "__main__":
-    out = getWeather()
-    temp = out.get('main').get('temp')
-    condition = out['weather'][0]['main']
-    pp.pprint(temp)
-    pp.pprint(condition)
+    # out = getWeather()
+    # temp = out.get('main').get('temp')
+    # condition = out['weather'][0]['main']
+    # max = out.get('main').get('temp_max')
+    # min = out.get('main').get('temp_min')
+
+    print(random.choice(getNews()))
+    # print(createResultString(getWeather()))
