57ec55d0614df1da75956ece81759d962a5d8f97
Cole Nelson <nelso345@purdue.edu>
2018-04-26 04:28:59 -0400

    Client working for internet information

--- sdClient.py
+++ sdClient.py
@@ -6,19 +6,35 @@ import random
 import serial
 import pprint as pp
 import datetime
+from time import sleep
 
 class Client:
     def __init__(self):
-        self.weather = getWeather()
-        self.headlines = getNews()
+        self.err = "ERR\n"
         self.rtc = datetime.datetime.now()
         self.port = serial.Serial(
-            port = '\dev\serial0',
+            port = '/dev/serial0',
             baudrate = 115200,
-            timeout = 0
+            timeout = None,
+            bytesize = serial.EIGHTBITS,
+            parity = serial.PARITY_NONE
         )
-        self.port.open()
-        self.updated = False
+        if(not self.port.isOpen()):
+            self.port.open()
+        
+        #self.weather = "60,65,55,Cloudy";
+        #self.orig = ["Trump gives birth", "Trump gives birth again, except this time in 3D"]
+        #self.headlines = [x for x in self.orig if len(x) < 25]
+        try:
+            self.weather = getWeather();
+            self.headlines = getNews();
+            self.headlines = [x for x in self.headlines if len(x) <= 60]
+            print(self.headlines)
+            self.updated = False
+        except:
+            self.write_serial(self.err)
+            print("Error caught!")
+
 
     def update(self):
         curr = datetime.datetime.now()
@@ -27,10 +43,11 @@ class Client:
             self.weather = getWeather()
             tmp = getNews()
             self.headlines = getNews()
+            print("Updated.")
 
     def write_serial(self, data_string):
-        self.port.write(data_string)
-        self.port.write("\n".encode())
+        self.port.write(data_string.encode())
+        self.port.write("\0".encode())
 
 
 def getWeather():
@@ -45,15 +62,15 @@ def getWeather():
     weather_dict = json.loads(out)
     weather.close()
 
-    return weather_dict
+    return createResultString(weather_dict)
 
 def createResultString(response_dict):
-    high = str(response_dict.get('main').get('temp_max'))
-    low = str(response_dict.get('main').get('temp_min'))
-    current = str(response_dict.get('main').get('temp'))
+    high = str(int(response_dict.get('main').get('temp_max')))
+    low = str(int(response_dict.get('main').get('temp_min')))
+    current = str(int(response_dict.get('main').get('temp')))
     conditions = response_dict['weather'][0]['main']
 
-    return high + "," + low + "," + current + "," + conditions
+    return current + "," + high + "," + low + "," + conditions
 
 def getNews():
     url = ('https://newsapi.org/v2/top-headlines?'
@@ -70,31 +87,44 @@ def getNews():
 
     return titles
 
-def uart_setup():
-    ser = serial.Serial()
-    ser.port = '/dev/serial0'
-    ser.baudrate = 115200
-    ser.timeout = 5
-    ser.open()
-
-    ser.write("Hello World!\n".encode())
-    message = ser.readline()
-    ser.write("You send: ".encode() + message)
-    ser.write("\nBye!".encode())
-
 def main_loop(client):
-    while True:
-        out = client.port.read(5) # 5 byte control words
+    while(True):
+        control_word = client.port.read(4)
+        print(control_word)
+        print(control_word == b'UPDT')
+        sleep(.1)
+        client.port.flush()
+        if control_word == b'UPDT':
+            try:
+                client.write_serial(client.weather + ',' + client.headlines[-1])
+                print(client.weather + ',' + client.headlines[-1])
+                print("Data fetched")
+                client.headlines.pop()
+            except IndexError:
+                client.write_serial(client.weather + ',' + "I hope you're having a great day!")
+        
+        if control_word == b'TIME':
+            client.write_serial(client.rtc.strftime("%Y,%m,%d,%H,%M,%S"))
+            sleep(.1)
+            client.port.flush()
+            print("Time written.")
+
+        if str(control_word) == 'MUSC':
+            subprocess.run(['play', 'temp.wav'])
+
+        sleep(.1)
+        client.port.flush()
         client.update()
 
-        if out:
 
 
 
 
 
 def init():
-    return Client()
+    client = Client()
+    return client
+
 
 if __name__ == "__main__":
     # out = getWeather()
